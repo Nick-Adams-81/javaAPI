4f7adfd867ca0f50d6d7fbeab46245ea57cf7105
Nick Adams <nick.pappas.adams@gmail.com>
2022-05-23 14:10:42 -0500

    added test user table

---
+++ src/main/java/com/example/javaapi/Models/User.java
@@ -0,0 +1,44 @@
+package com.example.javaapi.Models;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String email;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
