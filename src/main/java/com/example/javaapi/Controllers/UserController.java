d6b12e1d12484bc1bcad0b59ffa25c5a910eb699
Nick Adams <nick.pappas.adams@gmail.com>
2022-05-24 14:29:09 -0500

    added test template

--- src/main/java/com/example/javaapi/Controllers/UserController.java
+++ src/main/java/com/example/javaapi/Controllers/UserController.java
@@ -1,7 +1,10 @@
 package com.example.javaapi.Controllers;
 
+import com.example.javaapi.Models.User;
 import com.example.javaapi.Repositories.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
@@ -10,6 +13,12 @@ public class UserController {
 
     public UserController(UserRepository userDao) {
         this.userDao = userDao;
+    }
+
+    @GetMapping("/")
+    public String showUser(Model model) {
+        model.addAttribute("user", new User());
+        return "user";
     }
 
 
